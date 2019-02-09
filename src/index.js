b4591e3e89022882094b6f58eacf8d8a5be8a0b6
tiffanyadu <tadofdesign@gmail.com>
2019-02-09 08:07:31 -0600

    Create index.js and import React and ReactDOM libraries

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// Import the React and ReactDOM libraries
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Create a React component
+
+
+// Take the React component and show it on the screen
