ea0c63045e2ede87e983c0797b28505a549f40ad
tiffanyadu <tadofdesign@gmail.com>
2019-02-09 08:15:34 -0600

    Create a React componenent and render it using ReactDOM

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // Create a React component
-
+const App = () => {
+  return <div>Hi there!</div>;
+};
 
 // Take the React component and show it on the screen
+ReactDOM.render(
+  <App/>,
+  document.querySelector('#root')
+);
